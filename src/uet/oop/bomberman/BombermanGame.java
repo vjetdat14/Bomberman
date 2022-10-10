c00d6371e1c8051e5511a3f1aa0c361579241dd7
Bùi Hiếu <buihieuhip@gmail.com>
2022-10-10 13:23:46 +0700

    them thu vien

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -7,24 +7,24 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
-import uet.oop.bomberman.entities.Bomber;
-import uet.oop.bomberman.entities.Entity;
-import uet.oop.bomberman.entities.Grass;
-import uet.oop.bomberman.entities.Wall;
-import uet.oop.bomberman.graphics.Sprite;
+//import uet.oop.bomberman.entities.Bomber;
+//import uet.oop.bomberman.entities.Entity;
+//import uet.oop.bomberman.entities.Grass;
+//import uet.oop.bomberman.entities.Wall;
+//import uet.oop.bomberman.graphics.Sprite;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 public class BombermanGame extends Application {
-    
+
     public static final int WIDTH = 40;
     public static final int HEIGHT = 30;
-    
-    private GraphicsContext gc;
-    private Canvas canvas;
-    private List<Entity> entities = new ArrayList<>();
-    private List<Entity> stillObjects = new ArrayList<>();
+
+//    private GraphicsContext gc;
+//    private Canvas canvas;
+//    private List<Entity> entities = new ArrayList<>();
+//    private List<Entity> stillObjects = new ArrayList<>();
 
 
     public static void main(String[] args) {
@@ -34,12 +34,12 @@ public class BombermanGame extends Application {
     @Override
     public void start(Stage stage) {
         // Tao Canvas
-        canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
-        gc = canvas.getGraphicsContext2D();
+//        canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
+//        gc = canvas.getGraphicsContext2D();
 
         // Tao root container
         Group root = new Group();
-        root.getChildren().add(canvas);
+//        root.getChildren().add(scene);
 
         // Tao scene
         Scene scene = new Scene(root);
@@ -48,43 +48,44 @@ public class BombermanGame extends Application {
         stage.setScene(scene);
         stage.show();
 
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long l) {
-                render();
-                update();
-            }
-        };
-        timer.start();
-
-        createMap();
-
-        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
-        entities.add(bomberman);
-    }
-
-    public void createMap() {
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT; j++) {
-                Entity object;
-                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
-                    object = new Wall(i, j, Sprite.wall.getFxImage());
-                }
-                else {
-                    object = new Grass(i, j, Sprite.grass.getFxImage());
-                }
-                stillObjects.add(object);
-            }
-        }
-    }
-
-    public void update() {
-        entities.forEach(Entity::update);
-    }
-
-    public void render() {
-        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        stillObjects.forEach(g -> g.render(gc));
-        entities.forEach(g -> g.render(gc));
+//        AnimationTimer timer = new AnimationTimer() {
+//            @Override
+//            public void handle(long l) {
+//                render();
+//                update();
+//            }
+//        };
+//        timer.start();
+//
+//        createMap();
+//
+//        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
+//        entities.add(bomberman);
+//    }
+//
+//    public void createMap() {
+//        for (int i = 0; i < WIDTH; i++) {
+//            for (int j = 0; j < HEIGHT; j++) {
+//                Entity object;
+//                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
+//                    object = new Wall(i, j, Sprite.wall.getFxImage());
+//                }
+//                else {
+//                    object = new Grass(i, j, Sprite.grass.getFxImage());
+//                }
+//                stillObjects.add(object);
+//            }
+//        }
+//    }
+//
+//    public void update() {
+//        entities.forEach(Entity::update);
+//    }
+//
+//    public void render() {
+//        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+//        stillObjects.forEach(g -> g.render(gc));
+//        entities.forEach(g -> g.render(gc));
+//    }
     }
 }
